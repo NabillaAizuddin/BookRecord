a0862bd3704c431b86f2292310c89f9519e533cb
Nabilla Aizuddin <nuraisyanabilla@gmail.com>
2023-01-30 17:44:32 +0800

    upload files

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        // Set ID as primary key
+        [Key]
+        public int Id { get; set; }
+        // Make name required
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage ="The number keyed-in is out of range.")]
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
